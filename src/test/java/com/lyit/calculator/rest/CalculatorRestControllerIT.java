48acda3e3a6c21e6752c3be2447476f38f92653d
damiengallagher <damien.gallagher@gmail.com>
2021-03-07 13:06:18 +0000

    Update to add the deploy to ecs step

--- src/test/java/com/lyit/calculator/rest/CalculatorRestControllerIT.java
+++ src/test/java/com/lyit/calculator/rest/CalculatorRestControllerIT.java
@@ -1,15 +1,13 @@
 package com.lyit.calculator.rest;
 
-import com.lyit.calculator.service.ICalculatorService;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class CalculatorRestControllerIT {
